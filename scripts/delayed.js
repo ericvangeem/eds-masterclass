a198d6447b97fdec72cbe142423ca231b168e2b2
Eric VanGeem <eric.vangeem@herodigital.com>
2025-11-10 14:14:02 -0800

    move lib to delayed.js.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,7 @@
 // add delayed functionality here
+
+// Load DOMPurify library
+const script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/dompurify@3.0.6/dist/purify.js';
+script.setAttribute('nonce', 'aem');
+document.head.appendChild(script);
